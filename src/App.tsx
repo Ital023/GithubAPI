8675dc8cdf0132f6a49d5fd251d6d55c77bc9d32
Ital023 <contadoitalo911@gmail.com>
2024-08-16 19:40:06 -0300

    feat: set root colours

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+
 export default function App() {
   return (
+    <BrowserRouter>
+      <Routes>
+        
 
+
+      </Routes>
+    </BrowserRouter>
   );
 }
