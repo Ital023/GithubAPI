137e0d6989dbadf2281d54eab54bed564a373a40
Ital023 <contadoitalo911@gmail.com>
2024-08-17 00:38:27 -0300

    feat: axios get request

---
+++ src/models/userDTO.ts
@@ -0,0 +1,7 @@
+export type userDTO = {
+    avatar_url: string;
+    url: string;
+    followers: number;
+    location: string;
+    name: string;
+}
--- src/routes/Home/SearchUserGitHub/SearchUserGithubResult/index.tsx
+++ src/routes/Home/SearchUserGitHub/SearchUserGithubResult/index.tsx
@@ -1,31 +1,51 @@
 import "./styles.css";
-import { useParams } from "react-router-dom";
-import imgTeste from "../../../../assets/imgTeste.png";
+import { useNavigate, useParams } from "react-router-dom";
+import * as userService from "../../../../services/user-service"
+import { useEffect, useState } from "react";
+import { userDTO } from "../../../../models/userDTO";
 
 export default function SearchUserGitHubResult() {
   const params = useParams();
+
+  const navigate = useNavigate();
+
+  const[user, setUser] = useState<userDTO>();
+
+  useEffect(()=>{
+
+    userService.findByUsername(params.user)
+    .then(response => {
+      console.log(response);
+      
+      setUser(response.data)
+    })
+    .catch(()=> {
+      navigate("/");
+    })
+
+  },[user])
 
   return (
     <div className="gapi-user-container">
       <div className="gapi-user-result-container">
         <div className="gapi-user-result-left">
-          <img src={imgTeste} alt="imagem do github" />
+          <img src={user?.avatar_url} alt="imagem do github" />
         </div>
 
         <div className="gapi-user-result-right">
           <h2>Informações</h2>
           <ul>
             <li>
-              <p>Perfil: <span className="gapi-user-result-right-infos">https://api.github.com/users/acenelio</span></p>
+              <p>Perfil: <span className="gapi-user-result-right-infos">{user?.url}</span></p>
             </li>
             <li>
-              <p>Seguidores:<span className="gapi-user-result-right-infos"> 4379</span></p>
+              <p>Seguidores:<span className="gapi-user-result-right-infos"> {user?.followers}</span></p>
             </li>
             <li>
-              <p>Localidade: <span className="gapi-user-result-right-infos">Uberlândia</span></p>
+              <p>Localidade: <span className="gapi-user-result-right-infos">{user?.location}</span></p>
             </li> 
             <li>
-              <p>Nome: <span className="gapi-user-result-right-infos">Nelio Alves</span></p>
+              <p>Nome: <span className="gapi-user-result-right-infos">{user?.name}</span></p>
             </li>
           </ul>
         </div>
