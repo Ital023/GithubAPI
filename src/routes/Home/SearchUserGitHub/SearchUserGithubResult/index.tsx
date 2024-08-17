9942feba08166c1ec7d72adc970ea73b48726163
Ital023 <contadoitalo911@gmail.com>
2024-08-16 23:50:48 -0300

    feat: Card Search Result

--- src/routes/Home/SearchUserGitHub/SearchUserGithubResult/index.tsx
+++ src/routes/Home/SearchUserGitHub/SearchUserGithubResult/index.tsx
@@ -1,9 +1,35 @@
+import "./styles.css";
 import { useParams } from "react-router-dom";
+import imgTeste from "../../../../assets/imgTeste.png";
 
 export default function SearchUserGitHubResult() {
-    const params = useParams();
+  const params = useParams();
 
-    return (
-        <h2>{params.user}</h2>
-    );
+  return (
+    <div className="gapi-user-container">
+      <div className="gapi-user-result-container">
+        <div className="gapi-user-result-left">
+          <img src={imgTeste} alt="imagem do github" />
+        </div>
+
+        <div className="gapi-user-result-right">
+          <h2>Informações</h2>
+          <ul>
+            <li>
+              <p>Perfil: <span className="gapi-user-result-right-infos">https://api.github.com/users/acenelio</span></p>
+            </li>
+            <li>
+              <p>Seguidores:<span className="gapi-user-result-right-infos"> 4379</span></p>
+            </li>
+            <li>
+              <p>Localidade: <span className="gapi-user-result-right-infos">Uberlândia</span></p>
+            </li> 
+            <li>
+              <p>Nome: <span className="gapi-user-result-right-infos">Nelio Alves</span></p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
